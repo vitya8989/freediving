e151ff427542f185354fa33fc55e5f58c2b83c0f
viktor <komarov.v@triadasite.ru>
2023-06-29 03:51:38 +0300

    правки + отправка на почту

--- freeDiving/form-action.php
+++ freeDiving/form-action.php
@@ -1,17 +1,11 @@
 <?php
 header("Content-Type: text/html; charset=utf-8");
 
-$name = empty($_POST['name']) ? '' : $_POST['name'];
+$name = empty($_POST['name']) ? 'Клиент не представился' : $_POST['name'];
 $phone = empty($_POST['tel']) ? '' : $_POST['tel'];
-$comment = empty($_POST['question']) ? '' : $_POST['question'];
+$comment = empty($_POST['question']) ? 'Клиент не задал вопрос' : $_POST['question'];
 
-if (!empty($_POST['question']) && !empty($_POST['name'])) {
-	$fileContent = "Телефон для связи = ". $phone . ", имя = " . $name . ", вопрос клиента = " . $comment;
-} else if (!empty($_POST['name'])) {
-	$fileContent = "Телефон для связи = ". $phone . ", имя = " . $name;
-} else {
-	$fileContent = "Телефон для связи = ".$phone;
-}
+$fileContent = "Телефон для связи = ". $phone . ", имя = " . $name . ", вопрос клиента = " . $comment;
 
 mail("vitya898989@gmail.com", "Вопрос с лендинга free diving", $fileContent, "From: kayakingfuerteventura@gmail.com");
 
